5712e27bc15a9752f6930662fe8e18c606aff9b6
RishDvi <1221101095@student.mmu.edu.my>
2025-11-17 10:49:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=15, format='%(message)s')
 
 env = clips.Environment()
 router = clips.LoggingRouter()
-env.add)router(router)
+env.add_router(router)
 
 #input
 name = st.text_input("Enter your name")
